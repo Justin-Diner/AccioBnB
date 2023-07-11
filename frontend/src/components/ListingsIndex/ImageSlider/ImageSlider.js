7e9cead657066b185733b228c001d2add289014d
Justin <Jcdiner@gmail.com>
2023-07-11 12:21:46 -0400

    working on sizing

---
+++ frontend/src/components/ListingsIndex/ImageSlider/ImageSlider.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Slider from 'react-slick';
+import './ImageSlider.css'
+import './Slick.css'
+
+const sliderSettings = { 
+    dots: true,
+    infinite: true,
+    speed: 250,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    className: 'slides',
+  };
+
+const ImageSlider = ({photoUrls}) => {
+    return (
+        <div id="image_slider_wrapper">
+            <Slider {...sliderSettings} id="image_slider">
+                {photoUrls && (
+                    photoUrls.map((photoUrl) => 
+                        <div id="listing_index_slider_wrapper">
+                            <img className="listing_index_slider_image" src={photoUrl}></img>
+                        </div>
+                    )
+                )}
+            </Slider>
+        </div>
+    )
+}
+
+export default ImageSlider
--- frontend/src/components/ListingsIndex/ListingIndexItem/ListingIndexItem.js
+++ frontend/src/components/ListingsIndex/ListingIndexItem/ListingIndexItem.js
@@ -1,32 +1,31 @@
 import './ListingIndexItem.css';
 import { Link } from 'react-router-dom';
 import { calculateRating } from '../../utils/Utils';
+import ImageSlider from '../ImageSlider/ImageSlider';
 
 const ListingIndexItem = ({listing}) => {
-	let titlePhoto = listing?.photosUrl[0];
+	let titlePhoto = listing?.photosUrl;
 
 	return ( 
-		<Link to={`/listings/${listing.id}`}>
-		<div id="listing_index_item_container">
-				<div id="listing_index_item_image_wrapper"> 
-					<img id="listing_index_item_image" src={titlePhoto}></img>
-				</div>
-			<div id="listing_index_item_wrapper">
-				<div className="listing_index_item_textcontent" id="listing_index_item_state_and_ratings_wrapper">
-					<div className="listing_index_item_textcontent" id="listing_index_item_title_and_state">{`${listing.city}, ${listing.state}`}</div>
-					<div className="listing_index_item_textcontent" id="listing_index_item_rating">&#9733; {calculateRating(listing.overallRating)}</div>
-				</div>
-				<div className="listing_index_item_textcontent" id="listing_index_item_milesaway">{(Math.floor(Math.random() * 130) + 1).toString()} miles away</div>
-				<div className="listing_index_item_textcontent" id="listing_index_item_daterange">Dec 10 - 15</div>
-				<div className="listing_index_item_textcontent" id="listing_index_item_nightlyprice">
-					<div id="listing_index_item_price_only">
-						${`${listing.nightlyPrice.toLocaleString()}`}
-					</div>
-					<div>
-						night
-					</div>
-				</div>
-			</div>
+		<Link id="listing_index_item_link" to={`/listings/${listing.id}`}>
+		    <div id="listing_index_item_container">
+                <div id="listing_index_item_image_wrapper">
+                    <ImageSlider photoUrls={titlePhoto} /> 
+                </div>
+                <div className="listing_index_item_textcontent" id="listing_index_item_state_and_ratings_wrapper">
+                    <div className="listing_index_item_textcontent" id="listing_index_item_title_and_state">{`${listing.city}, ${listing.state}`}</div>
+                    <div className="listing_index_item_textcontent" id="listing_index_item_rating">&#9733; {calculateRating(listing.overallRating)}</div>
+                </div>
+                <div className="listing_index_item_textcontent" id="listing_index_item_milesaway">{(Math.floor(Math.random() * 130) + 1).toString()} miles away</div>
+                <div className="listing_index_item_textcontent" id="listing_index_item_daterange">Dec 10 - 15</div>
+                <div className="listing_index_item_textcontent" id="listing_index_item_nightlyprice">
+                    <div id="listing_index_item_price_only">
+                        ${`${listing.nightlyPrice.toLocaleString()}`}
+                    </div>
+                <div>
+                    night
+                </div>
+            </div>
 		</div>
 		</Link>
 	)

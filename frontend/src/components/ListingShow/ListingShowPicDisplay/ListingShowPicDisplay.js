eec642e48e6d4a9931adeee27b8f0b9299fa6e6b
Justin <Jcdiner@gmail.com>
2023-08-02 11:50:34 -0400

    changed grid template to 310

--- frontend/src/components/ListingShow/ListingShowPicDisplay/ListingShowPicDisplay.js
+++ frontend/src/components/ListingShow/ListingShowPicDisplay/ListingShowPicDisplay.js
@@ -16,19 +16,19 @@ const ListingShowPicDisplay = ({listing}) => {
 	return (
 		<div id="LSPD_wrapper">
 			<div className="LSPD_picture_item" id="LSPD_item_1" >
-				<img className="listing_index_item_image" src={listingUrls[0]}></img>
+				<img className="listing_index_item_image" alt="image_1" src={listingUrls[0]}></img>
 			</div>
 			<div className="LSPD_picture_item" id="LSPD_item_2" >
-				<img className="listing_index_item_image" src={listingUrls[1]}></img>
+				<img className="listing_index_item_image" alt="image_2" src={listingUrls[1]}></img>
 			</div>
 			<div className="LSPD_picture_item" id="LSPD_item_3" >
-				<img className="listing_index_item_image" src={listingUrls[2]}></img>
+				<img className="listing_index_item_image" alt="image_3" src={listingUrls[2]}></img>
 			</div>
 			<div className="LSPD_picture_item" id="LSPD_item_4" >
-				<img className="listing_index_item_image" src={listingUrls[3]}></img>
+				<img className="listing_index_item_image" alt="image_4" src={listingUrls[3]}></img>
 			</div>
 			<div className="LSPD_picture_item" id="LSPD_item_5" >
-				<img className="listing_index_item_image" src={listingUrls[4]}></img>
+				<img className="listing_index_item_image" alt="image_5" src={listingUrls[4]}></img>
 			</div>
 		</div> 
 	)
